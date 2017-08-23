ac588ffe11f14de76aef6ad6be90cd06b0e98a9e
Piti <piti118@gmail.com>
2017-08-23 12:38:37 +0700

    update

--- stalker-flask/app.py
+++ stalker-flask/app.py
@@ -17,5 +17,18 @@ def yes_no():
     return jsonify({"answer": answer})
 
 
+@app.route('/versus', methods=['POST'])
+def versus():
+    player1 = request.json['player1']
+    player2 = request.json['player2']
+    score1 = hash(player1)
+    score2 = hash(player2)
+    winner, loser = (player1, player2) if score1 > score2 else (player2, player1)
+    return jsonify({
+        "winner": {"name": winner, "score": hash(winner)},
+        "loser": {"name": loser, "score": hash(loser)},
+    })
+
+
 if __name__ == '__main__':
     app.run(debug=True)
