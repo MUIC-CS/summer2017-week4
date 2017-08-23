76bdcfb2eba2a4721aa973cd92bcfdfa4c4f7b33
Piti <piti118@gmail.com>
2017-08-23 13:19:52 +0700

    update

--- stalker-app/src/Versus.js
+++ stalker-app/src/Versus.js
@@ -4,7 +4,11 @@ import axios from 'axios'
 function NameBox({name, onNameChange}) {
   return (
     <div>
-      <input type="text" value={name} />
+      <input
+        type="text"
+        value={name}
+        onChange={(e)=>onNameChange(e.target.value)}
+      />
       <img
         src={`https://robohash.org/${name}`}
         style={{height:200, width:200}}
@@ -17,9 +21,30 @@ function NameBox({name, onNameChange}) {
 export default class Versus extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      player1: '',
+      player2: ''
+    }
+  }
+
+  onPlayer1NameChange(newName) {
+    this.setState({player1: newName})
   }
 
   render() {
-    return (<NameBox name="Piti"/>)
+    return (
+      <div>
+        <NameBox
+          name={this.state.player1}
+          onNameChange={(n)=>this.onPlayer1NameChange(n)}
+        />
+      </div>)
   }
 }
+//
+//
+///
+//
+//
+//
+///
